392b3fc1246d213ad31e8d2c8f4fc60ce4f3dc53
Joseph Santiago <santiago.jospeh92@csu.fullerton.edu>
2020-04-22 18:44:22 -0700

    challenges

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
+var mime = require('mime');
 
 var handleError = function (err, res) {
-    res.writeHead(404);
-    res.end();
-  };
+  file = '/error.html';
+  var fp = extract(file);
+  fs.readFile(fp, function (err, data){
+      res.end(data);
+  });
+}
 
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request.');
@@ -17,6 +21,7 @@ var server = http.createServer(function (req, res) {
         handleError(err, res);
         return;
       } else {
+        var type = mime.getType(filePath);
         res.setHeader('Content-Type', 'text/html');
     res.end(data);}
   });
